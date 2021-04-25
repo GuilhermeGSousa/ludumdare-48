0576dfac083393214c70d5f4ab896daf00fe9edb
thomas.pitiot <thomas.pitiot@fittingbox.com>
2021-04-25 19:05:37 +0200

    Update lights to fit the depth

--- Assets/DepthBehaviour.cs
+++ Assets/DepthBehaviour.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 using System;
-
+using UnityEngine.Experimental.Rendering.Universal;
 
 public enum DepthLayerNames // your custom enumeration
  {
@@ -23,16 +23,61 @@ public class DepthLayer {
 public class DepthBehaviour : MonoBehaviour {
 
     public static DepthBehaviour instance = null;
+    public DepthLayer[] layers;
 
+    public float maxPointLightIntensity = 2.04f;
+    public float maxFlahsLightIntensity = 2.95f;
+
+    [SerializeField] float noLightDepth;
+    [SerializeField] Light2D globalLight;
+    Submarine submarine;
+
+    GameObject [] pointLights;
+    GameObject [] flashLights;
+
+    float startY;
+
+    
+    private void Init()
+    {
+        submarine = GameObject.FindGameObjectWithTag("Player").GetComponent<Submarine>();
+        pointLights = GameObject.FindGameObjectsWithTag("PointsLight");
+        flashLights = GameObject.FindGameObjectsWithTag("FlashLights");
+        startY = submarine.gameObject.transform.position.y;
+    }
     private void Awake() {
         if(instance == null) instance = this;
         else
             Destroy (gameObject);
 
         DontDestroyOnLoad(gameObject);
+        Init();
     }
-    public DepthLayer[] layers;
 
+    
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 playerPosition = submarine.gameObject.transform.position;
+
+        float diffFactor =0.93f;
+        if(playerPosition.y > -noLightDepth)
+        {
+            diffFactor = Mathf.Abs(playerPosition.y - startY) / Mathf.Abs(noLightDepth - startY);
+            
+        }
+        globalLight.intensity = 1.0f-diffFactor;
+        foreach(GameObject obj in pointLights)
+        {
+            Light2D light = obj.GetComponent<Light2D>();
+            light.intensity = maxPointLightIntensity*diffFactor;
+        }
+        foreach(GameObject obj in flashLights)
+        {
+            Light2D light = obj.GetComponent<Light2D>();
+            light.intensity = maxFlahsLightIntensity*diffFactor;
+        }
+    }
     public DepthLayer getLayerAt(int depth) {
         foreach(DepthLayer layer in layers) {
             depth -= layer.span;
