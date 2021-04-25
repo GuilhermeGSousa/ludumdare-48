5796e0593e93ab1192816b2d7fccb7536791cba8
Guilherme Sousa <guilherme.sousa1994@gmail.com>
2021-04-25 15:14:38 +0200

    Add fish ai

--- Assets/FishBehaviour.cs
+++ Assets/FishBehaviour.cs
@@ -15,20 +15,25 @@ public class FishBehaviour : MonoBehaviour
 
     [SerializeField] FishType type;
     [SerializeField] DepthLayerNames[] predilectionLayers;
-    [SerializeField] float speed = 2.0f;
+    [SerializeField] float speed = 1.0f;
+    [SerializeField] float scaredSpeed = 5.0f;
     [SerializeField] float direction = 0.0f;
     [SerializeField] float rotationSpeed = 1.0f;
     [SerializeField] bool doFlips = false;
     [SerializeField] bool isFlipped = false;
+    [SerializeField] float maxSpeed = 0.5f;
+    [SerializeField] float submarineMinSpeed = 1f;
     float currentDirection = 0.0f;
     float inclinationT = 0.0f;
 
     float previousDirection = 0.0f;
     float previousSpeed = 0.0f;
-
+    bool canChooseDirection = true;
+    float newDirectionTime;
 
     Rigidbody2D rb2d;
-
+    Collider2D bounds;
+    CircleCollider2D closeArea;
 
     // Start is called before the first frame update
     void Start()
@@ -40,17 +45,25 @@ public class FishBehaviour : MonoBehaviour
         Vector2 minMaxDepth = DepthBehaviour.instance.getMinMaxDepth(spawningLayer);
         int y = Random.Range((int)minMaxDepth[0], (int)minMaxDepth[1]);
 
-        Collider2D bounds = GameObject.FindGameObjectWithTag("Bounds").GetComponent<Collider2D>();
+        bounds = GameObject.FindGameObjectWithTag("Bounds").GetComponent<Collider2D>();
         float x = Random.Range(bounds.bounds.min.x, bounds.bounds.max.x);
 
-        gameObject.transform.position = new Vector2(x, -y);
+        //gameObject.transform.position = new Vector2(x, -y);
 
         rb2d = GetComponent<Rigidbody2D> ();
+        closeArea = GetComponent<CircleCollider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        ModifyPhysics();
+
+        if(canChooseDirection)
+            StartCoroutine("ChooseDirection");
+
+        CorrectDirectionToBounds();
+
         if (Mathf.Approximately(currentDirection, direction))
                 inclinationT = 0.0f;
             else
@@ -66,24 +79,84 @@ public class FishBehaviour : MonoBehaviour
                 Flip();
             }
 
-            if (!Mathf.Approximately(previousSpeed, speed))
-                rb2d.AddRelativeForce(Quaternion.Euler(0, 0, currentDirection) * new Vector2(speed, 0.0f) );
+            rb2d.AddRelativeForce(Quaternion.Euler(0, 0, currentDirection) * new Vector2(speed, 0.0f) );
         }
         else 
         { 
             //Smooth rotation
             transform.rotation = Quaternion.Euler(0, 0, currentDirection);
 
-            if (!Mathf.Approximately(previousSpeed, speed))
-                rb2d.AddRelativeForce(new Vector2(speed, 0.0f) );
+            rb2d.AddRelativeForce(new Vector2(speed, 0.0f) );
         }
 
         previousDirection = currentDirection;
         previousSpeed = speed;
     }
+
+    private void CorrectDirectionToBounds()
+    {
+        Vector2 correctedDirection = Vector2.zero;
+
+        if(transform.position.x < bounds.bounds.min.x)
+        {
+            correctedDirection += Vector2.right;
+        }
+        else if(transform.position.x > bounds.bounds.max.y)
+        {
+            correctedDirection += Vector2.left;
+        }
+
+        if(transform.position.y < bounds.bounds.min.y)
+        {
+            correctedDirection += Vector2.down;
+        }
+        else if(transform.position.y > bounds.bounds.max.y)
+        {
+            correctedDirection += Vector2.up;
+        }
+
+        if(correctedDirection != Vector2.zero)
+        {
+            direction = Vector2.Angle(Vector2.right, correctedDirection);
+        }
+    }
+
     public void Flip()
     {
         isFlipped = !isFlipped;
         transform.rotation = Quaternion.Euler(0f, isFlipped ? 180f : 0f, 0f);
+    }
+
+    private void ModifyPhysics()
+    {
+        rb2d.velocity = Vector2.ClampMagnitude(rb2d.velocity, maxSpeed);
+    }
+
+
+    private IEnumerator ChooseDirection()
+    {
+        rb2d.velocity = rb2d.velocity * 0.8f;
+        canChooseDirection = false;
+        direction =  Random.Range(0, 360);
+        newDirectionTime = Random.Range(2, 5);
+        yield return new WaitForSeconds(newDirectionTime);
+        canChooseDirection = true;
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            Rigidbody2D submarine = other.gameObject.GetComponent<Rigidbody2D>();
+            
+            if(submarine.velocity.magnitude > submarineMinSpeed)
+            {
+                Vector2 escapeDirection = transform.position - other.gameObject.transform.position;
+
+                direction = Vector2.Angle(Vector2.right, escapeDirection);
+
+                rb2d.AddForce(escapeDirection * scaredSpeed);
+            }
+        }
     }
 }
